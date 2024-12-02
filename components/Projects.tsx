65f0c87fbb92a9867b0209c281910f82ee4764c4
Emil Ståhl <emil.stahl@icloud.com>
2024-12-02 20:52:34 +0100

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,7 +18,7 @@ const projects = [
     title: "Performance Analysis of the FRRouting Route Server",
     description: "Bachelor's Thesis researching the performance of FRRouting with a set of experiments using ExaBGP and Wireshark",
     image: "/images/distributed-fs.png",
-    tags: ["Go", "gRPC", "Raft Consensus", "Distributed Systems"],
+    tags: ["Bachelor's Thesis", "BGP", "FRRouting", "Wireshark", "ExaBGP", "Python", "Bash", "PBPG Parser"],
     link: "#"
   },
   {
