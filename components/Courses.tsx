5ec2ac6d5cf9020972b1068fbdfc44329a3eea62
Emil Ståhl <emil.stahl@icloud.com>
2024-12-03 23:02:35 +0100

    Update Courses.tsx

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -44,10 +44,10 @@ export default function Courses() {
         <div className="lg:text-center">
           <h2 className="text-base text-primary font-semibold tracking-wide uppercase">Education</h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            University Courses
+            KTH Royal Institute of Technology Courses
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            A comprehensive list of key courses completed during my academic journey
+            A comprehensive list of key courses completed during my academic journey at KTH Royal Institute of Technology
           </p>
         </div>
 
